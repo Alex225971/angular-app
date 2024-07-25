69ec3ec89c5cf05b429f351febb75aae864f8be7
Alex225971 <ahaines964@gmail.com>
2024-07-25 15:48:46 +0100

    Initial commit, baby API created

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        if(user == null) 
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+}
